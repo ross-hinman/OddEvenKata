23e6d9510f6607cfa2d74b5778323d86d61cabbe
Ross Hinman <hinman.ross@gmail.com>
2018-08-17 15:35:42 -0400

    Initial Commit

---
+++ OddEvenKata.Tests/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace OddEvenKata.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [TestCase(4)]
+        [TestCase(2)]
+        public void Should_Return_True_When_Number_Is_Divisible_By_2(int i)
+        {
+            var result = Number.IsEven(i);
+            Assert.AreEqual(true, result);
+        }
+        [TestCase(1)]
+        [TestCase(3)]
+        public void Should_Return_True_When_Number_Is_Odd(int i)
+        {
+            var result = Number.IsOdd(i);
+            Assert.AreEqual(true, result);
+        }
+        [TestCase(7)]
+        [TestCase(13)]
+        public void Should_Return_True_When_Number_Is_Prime(int i)
+        {
+            var result = Number.IsPrime(i);
+            Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ OddEvenKata/Number.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddEvenKata
+{
+    public static class Number
+    {
+        public static bool IsPrime(int i)
+        {
+            if (i == 2 || i == 3)
+            {
+                return true;
+            }
+
+            if (i % 2 == 0 || i % 3 == 0)
+            {
+                return false;
+            }
+
+            if (i < 2)
+            {
+                return false;
+            }
+
+            var ceiling = Math.Sqrt(i);
+            for (int j = 3; j <= ceiling; j += 2)
+            {
+                if (i % j == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool IsEven(int i)
+        {
+            if (i % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsOdd(int i)
+        {
+            if (i % 2 == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+        
+        public static string NumberToWord(int i)
+        {
+
+                 if (IsPrime(i))
+                {
+                    return "Prime";
+                }
+                else if (IsEven(i))
+                {
+                    return "Even";
+                }
+                else if (IsOdd(i))
+                {
+                    return "Odd";
+                }
+
+            return null;
+        }
+
+    }
+}
